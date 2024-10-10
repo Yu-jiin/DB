cd12b4e97cdfd3b305699311da86a080a8205dff
Yujiin <jiin4083@naver.com>
2024-10-10 10:19:42 +0900

    ORDER BY

--- 01-sql/01-single-table-queries.sql
+++ 01-sql/01-single-table-queries.sql
@@ -1,10 +1,33 @@
+-- Active: 1728520736769@@127.0.0.1@3306
 -- 01. Querying data
+SELECT LastName FROM employees;
+
+SELECT LastName, FirstName FROM employees;
+
+-- 전체 필드 조회
+SELECT * from employees;
+
+-- 필드 이름 바꾸기
+SELECT FirstName AS '이름' FROM employees;
+
+SELECT Name, Milliseconds / 60000 AS '재생 시간(분)' From tracks;
 
 -- 02. Sorting data
+SELECT FirstName from employees ORDER BY "FirstName" DESC;
+
+SELECT Country, City FROM customers ORDER BY Country DESC, City ASC;
+
+SELECT Name, Milliseconds / 60000 AS '재생 시간(분)'
+FROM tracks
+ORDER BY Milliseconds DESC;
+
 
 -- NULL 정렬 예시
+SELECT ReportsTo
+FROM employees
+ORDER BY ReportsTo;
 
 -- 03. Filtering data
 
--- 04. Grouping data
+-- 04. Grouping data 
 
