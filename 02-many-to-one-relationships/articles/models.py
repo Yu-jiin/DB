b3ffb66cb18b52eee7200d07d80c81b2e384556d
Yujiin <jiin4083@naver.com>
2024-10-11 09:30:33 +0900

    comment models.py

--- 02-many-to-one-relationships/articles/models.py
+++ 02-many-to-one-relationships/articles/models.py
@@ -7,3 +7,12 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    # 참조하려는 대상의 단수형을 외래키로
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
