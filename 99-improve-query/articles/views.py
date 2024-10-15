210c600f2482e14c1533545eeb4292a7e76b5298
Yujiin <jiin4083@naver.com>
2024-10-15 11:02:57 +0900

    query 최적화까지 끝

--- 99-improve-query/articles/views.py
+++ 99-improve-query/articles/views.py
@@ -5,8 +5,9 @@ from django.db.models import Count
 
 # Create your views here.
 def index_1(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.annotate(Count('comment')).order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    # 1대1 관계를 맺는 comment까지 가져오겠다 
+    articles = Article.objects.annotate(Count('comment')).order_by('-pk')
     context = {
         'articles': articles,
     }
@@ -14,8 +15,9 @@ def index_1(request):
 
 
 def index_2(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.select_related('user').order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    # Article 정보 조회하면서 연관된 user정보도 같이 ...
+    articles = Article.objects.select_related('user').order_by('-pk')
     context = {
         'articles': articles,
     }
@@ -23,8 +25,10 @@ def index_2(request):
 
 
 def index_3(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    # 역참조 일 때 
+    # Aritlce 객체 조회한 후 연관된 모든 comment_set 모두 미리 업로드 ..
+    articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
     context = {
         'articles': articles,
     }
@@ -35,11 +39,12 @@ from django.db.models import Prefetch
 
 
 def index_4(request):
-    articles = Article.objects.order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
     # articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
-    # articles = Article.objects.prefetch_related(
-    #     Prefetch('comment_set', queryset=Comment.objects.select_related('user'))
-    # ).order_by('-pk')
+    # ㄱㅔ시글 + 게시글 댓글 목록 + 작성자 한번에 조회 
+    articles = Article.objects.prefetch_related(
+        Prefetch('comment_set', queryset=Comment.objects.select_related('user'))
+    ).order_by('-pk')
 
     context = {
         'articles': articles,
